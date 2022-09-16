dac2226eb3ad5072051f4a59474da17b029a29db
Suganya Panneerselvam <suganya.panneerselvam@dataquest.com.au>
2022-09-16 11:10:23 +0530

    Connected database in dotnet core empty

--- dotNetCore_WebApplication/dotNetCoreWebAppEmpty/Startup.cs
+++ dotNetCore_WebApplication/dotNetCoreWebAppEmpty/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,10 +14,18 @@ namespace dotNetCoreWebAppEmpty
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,13 +36,21 @@ namespace dotNetCoreWebAppEmpty
                 app.UseDeveloperExceptionPage();
             }
 
+            string conString = Microsoft
+   .Extensions
+   .Configuration
+   .ConfigurationExtensions
+   .GetConnectionString(this.Configuration, "DefaultConnection");
+
+           
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(conString);
                 });
             });
         }
